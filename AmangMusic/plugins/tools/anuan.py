32d789b6e70b7b1747ac4c1a37fc58ec18d7825a
amanqs <116822442+amanqs@users.noreply.github.com>
2023-08-04 13:44:47 +0700

    Update anuan.py

--- AmangMusic/plugins/tools/anuan.py
+++ AmangMusic/plugins/tools/anuan.py
@@ -1,22 +1,17 @@
 import os
 import re
 import asyncio
-
-import importlib
-import sys
-from AmangMusic.core.bot import AmangBot
 import time
 from pyrogram import *
 from pyrogram.types import *
 from random import choice
-from AmangMusic import *
-from AmangMusic.plugins import ALL_MODULES
-from config.config import API_ID, API_HASH
+from AmangMusic import cloner, ASSUSERNAME, BOT_NAME
+from AmangMusic.config import API_ID, API_HASH
 IMG = ["https://telegra.ph/file/cefd3211a5acdcd332415.jpg", "https://telegra.ph/file/30d743cea510c563af6e3.jpg", "https://telegra.ph/file/f7ae22a1491f530c05279.jpg", "https://telegra.ph/file/2f1c9c98452ae9a958f7d.jpg"]
 MESSAGE = "Heya! I'm a music bot hoster/Cloner\n\nI can Host Your Bot On My Server within seconds\n\nTry /clone Token from @botfather"
 
-@app.on_message(filters.private & filters.command("startps"))
-async def hello(app, message: Message):
+@cloner.on_message(filters.private & filters.command("start"))
+async def hello(client, message: Message):
     buttons = [
            [
                 InlineKeyboardButton("✘ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url="t.me/TheUpdatesChannel"),
@@ -26,13 +21,13 @@ async def hello(app, message: Message):
             ],
             ]
     reply_markup = InlineKeyboardMarkup(buttons)
-    await app.send_photo(message.chat.id, f"{choice(IMG)}", caption=MESSAGE, reply_markup=reply_markup)
+    await client.send_photo(message.chat.id, f"{choice(IMG)}", caption=MESSAGE, reply_markup=reply_markup)
 
 ##Copy from here 
 
 # © By Itz-Zaid Your motherfucker if uh Don't gives credits.
-@app.on_message(filters.private & filters.command("clone"))
-async def clone(app, msg: Message):
+@cloner.on_message(filters.private & filters.command("clone"))
+async def clone(bot, msg: Message):
     chat = msg.chat
     text = await msg.reply("Usage:\n\n /clone token")
     cmd = msg.command
@@ -40,13 +35,12 @@ async def clone(app, msg: Message):
     try:
         await text.edit("Booting Your Client")
                    # change this Directry according to ur repo
-        client = AmangBot()
+        client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "AmangMusic.plugins"})
         await client.start()
-        for all_module in ALL_MODULES:
-            importlib.import_module("AmangMusic.plugins" + all_module)
         user = await client.get_me()
-        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Babayo")
+        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @{ASSUSERNAME} To Your Chat!\n\nThanks for Cloning.")
     except Exception as e:
         await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
 #End
 ##This code fit with every pyrogram Codes just import then @Client Xyz!
+
